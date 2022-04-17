650d153b032f2fdf87bce336d214e6d752957feb
Tatvesh <tattu24112000@gmail.com>
2022-04-18 00:27:58 +0530

    first-commit

---
+++ src/components/Tempapp.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './css/style.css'
+import {useState, useEffect} from 'react'
+
+const Tempapp = () => {
+
+    const [city, setCity] = useState(null);
+    const [ search, setSearch] = useState('Goa')
+
+    useEffect(() => {
+      
+        const fetchApi = async () => {
+            const url =`https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=7bee37a59c3124a060f21228ea5bbf7f`
+            const response = await fetch(url)
+            const resJSON = await response.json()
+            console.log(resJSON);
+            setCity(resJSON.main)
+        }
+      
+        fetchApi();
+    
+      },[search]
+    )
+    
+
+  return (
+    <div>
+        <div className="box">
+            <div className="inputData">
+                <input type='search' className='inputField' value={search} onChange={ (event) => {
+                    setSearch(event.target.value)
+                }} />
+            </div>
+
+            {
+                !city ? (
+                    <p>No Data Found</p>
+                ) :
+
+        <div className="info">
+            <h2 className="location">
+                {search}
+            </h2>
+            <h1 className="temp">
+                {city.temp} °C
+            </h1>
+            {/* <h3 className="tempmin-max">
+
+            </h3> */}
+        </div>
+
+
+            }
+
+        </div>
+    </div>
+  )
+}
+
+export default Tempapp
